0454f61944662527c376ab06711691d8621b8379
Halil ibrahim BAYAT <hibrahimbyt@gmail.com>
2022-11-19 14:47:25 +0300

    add new modules

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express           =   require("express");
 const bodyParser        =   require("body-parser");
 const ejs               =   require("ejs");
 const app               =   express();
+const  _                =   require("lodash")
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -9,7 +10,8 @@ app.set("view engine","ejs");
 
 // my objects
 let myNewContentHeaders  = [];
-let myNewContents = []
+let myNewContents = [];
+let postObjectsList = [];
 
 // default block texts
 const content = "Learning new things is a huge part of life -- we should always be striving to grow and learn a new skill."+
@@ -22,33 +24,56 @@ const about   = "During undergraduate and graduate studies in Mechatronics Engin
 "to work on during master's degree, chose to develop and advance myself in this field. During graduate education, became someone who is eager to learn and knows how to learn."+
 "After all, chose the field of artificial intelligence as PhD field of study. Wants to improve myself and develop useful applications in this field in business life as well.";
 
+//creating home page response
 app.get("/",function(req,res){
     res.render("home",{myHomePage:"Home",
     content1:content,
     content:myNewContents,
-    contentHeader:myNewContentHeaders});        
-    console.log(myNewContentHeaders);
-    console.log(myNewContents);
-})
+    contentHeader:myNewContentHeaders});   
+});
+
+//about page response
 app.get("/about",function(req,res){
     res.render("about",{myHomePage:"About",content1:about})
 })
+
+//contact page response
 app.get("/contact",function(req,res){
     res.render("contact",{myHomePage:"Contact"})
 })
+
+//compose page response
 app.get("/compose",function(req,res){
     res.render("compose",{myHomePage:"Compose New Content"})
-})
+});
+
+
+//compose post response
 app.post("/compose",function(req,res){
     let newContentHeader = req.body.contentHeader;
     let newContent       = req.body.content;
     myNewContents.push(newContent);
     myNewContentHeaders.push(newContentHeader);
+    postObjectsList.push({header:newContentHeader,content:newContent});
     res.redirect("/");
 })
 
+// routing params:
+app.get("/posts/:postId",function(req,res){
+    const theTitle = _.lowerCase(req.params.postId);
 
-app.listen(8800,()=>{
+    postObjectsList.forEach(function(object){ 
+        const savedTitle =   _.lowerCase(object.header); 
+            
+        if(theTitle === savedTitle){
+            res.render("posts",{myHomePage:"The Post",
+            postHeader:object.header,postContent:object.content});            
+        }
+        
+    })
+})
+
+app.listen(8800,function(){
     console.log("The server is running on port 8800");
     
 })
